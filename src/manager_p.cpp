KDE/bluez-qt

commit 0b50f8ab4a6a22189dfbdf9cd60a65c5adc69007

ObjectManager::GetManagerObjects also returns properties of all objects
For some reason, I missed this very important fact...

It is then no longer needed to have InitAdaptersJob and LoadDeviceJob,
both Adapters and Devices will have the properties loaded in init.

src/manager_p.cpp
@@ -119,9 +119,9 @@ void ManagerPrivate::getManagedObjectsFinished(QDBusPendingCallWatcher *watcher)
         const QVariantMapMap &interfaces = it.value();
 
         if (interfaces.contains(Strings::orgBluezAdapter1())) {
-            addAdapter(path);
+            addAdapter(path, interfaces.value(Strings::orgBluezAdapter1()));
         } else if (interfaces.contains(Strings::orgBluezDevice1())) {
-            addDevice(path, interfaces.value(Strings::orgBluezDevice1()).value(QStringLiteral("Adapter")).value<QDBusObjectPath>().path());
+            addDevice(path, interfaces.value(Strings::orgBluezDevice1()));
         } else if (interfaces.contains(Strings::orgBluezAgentManager1())) {
             m_bluezAgentManager = new BluezAgentManager(Strings::orgBluez(), path, QDBusConnection::systemBus(), this);
         }
@@ -209,9 +209,9 @@ void ManagerPrivate::interfacesAdded(const QDBusObjectPath &objectPath, const QV
 
     for (it = interfaces.constBegin(); it != interfaces.constEnd(); ++it) {
         if (it.key() == Strings::orgBluezAdapter1()) {
-            addAdapter(path);
+            addAdapter(path, it.value());
         } else if (it.key() == Strings::orgBluezDevice1()) {
-            addDevice(path, it.value().value(QStringLiteral("Adapter")).value<QDBusObjectPath>().path());
+            addDevice(path, it.value());
         }
     }
 }
@@ -226,16 +226,6 @@ void ManagerPrivate::interfacesRemoved(const QDBusObjectPath &objectPath, const 
         } else if (interface == Strings::orgBluezDevice1()) {
             removeDevice(path);
         }
-    }
-}
-
-void ManagerPrivate::adapterLoaded(AdapterPrivate *adapter)
-{
-    Q_EMIT q->adapterAdded(adapter->q);
-
-    if (!m_usableAdapter && adapter->q->isPowered()) {
-        m_usableAdapter = adapter->q;
-        Q_EMIT q->usableAdapterChanged(m_usableAdapter);
     }
 }
 
@@ -267,23 +257,30 @@ void ManagerPrivate::adapterPoweredChanged(bool powered)
     }
 }
 
-void ManagerPrivate::addAdapter(const QString &adapterPath)
+void ManagerPrivate::addAdapter(const QString &adapterPath, const QVariantMap &properties)
 {
-    Adapter *adapter = new Adapter(adapterPath, this);
+    Adapter *adapter = new Adapter(adapterPath, properties, this);
     m_adapters.insert(adapterPath, adapter);
     connect(adapter, &Adapter::poweredChanged, this, &ManagerPrivate::adapterPoweredChanged);
 
-    if (m_adaptersLoaded) {
-        adapter->d->load();
-        connect(adapter->d, &AdapterPrivate::loaded, this, &ManagerPrivate::adapterLoaded);
+    // We will only emit adapterAdded/usableAdapterChanged after loaded (for newly added adapters)
+    if (!m_loaded) {
+        return;
+    }
+
+    Q_EMIT q->adapterAdded(adapter);
+
+    if (!m_usableAdapter && adapter->isPowered()) {
+        m_usableAdapter = adapter;
+        Q_EMIT q->usableAdapterChanged(m_usableAdapter);
     }
 }
 
-void ManagerPrivate::addDevice(const QString &devicePath, const QString &adapterPath)
+void ManagerPrivate::addDevice(const QString &devicePath, const QVariantMap &properties)
 {
-    Adapter *adapter = m_adapters.value(adapterPath);
+    Adapter *adapter = m_adapters.value(properties.value(QStringLiteral("Adapter")).value<QDBusObjectPath>().path());
     Q_ASSERT(adapter);
-    Device *device = new Device(devicePath, adapter, this);
+    Device *device = new Device(devicePath, properties, adapter, this);
     adapter->d->addDevice(device);
     m_devices.insert(devicePath, device);
 }

autotests/adaptertest.cpp
@@ -12,7 +12,7 @@ using namespace QBluez;
 void AdapterTest::initTestCase()
 {
     Manager *manager = new Manager();
-    InitManagerJob *initJob = manager->init(Manager::InitManagerAndAdapters);
+    InitManagerJob *initJob = manager->init();
     initJob->exec();
     QVERIFY(!initJob->error());
 

autotests/devicetest.cpp
@@ -2,7 +2,6 @@
 #include "autotests.h"
 #include "pendingcall.h"
 #include "initmanagerjob.h"
-#include "loaddevicejob.h"
 
 #include <QtTest/QTest>
 #include <QtTest/QSignalSpy>
@@ -13,7 +12,7 @@ using namespace QBluez;
 void DeviceTest::initTestCase()
 {
     Manager *manager = new Manager();
-    InitManagerJob *initJob = manager->init(Manager::InitManagerAndAdapters);
+    InitManagerJob *initJob = manager->init();
     initJob->exec();
     QVERIFY(!initJob->error());
 
@@ -22,11 +21,6 @@ void DeviceTest::initTestCase()
 
         Q_FOREACH (Device *device, adapter->devices()) {
             QVERIFY(!device->ubi().isEmpty());
-
-            LoadDeviceJob *deviceJob = device->load();
-            deviceJob->exec();
-            QVERIFY(deviceJob->error() == LoadDeviceJob::NoError);
-            QVERIFY(device->isLoaded());
 
             DeviceUnit u;
             u.device = device;

autotests/jobstest.cpp
@@ -1,11 +1,9 @@
 #include "jobstest.h"
 #include "manager.h"
 #include "initmanagerjob.h"
-#include "initadaptersjob.h"
 #include "obexmanager.h"
 #include "initobexmanagerjob.h"
 #include "device.h"
-#include "loaddevicejob.h"
 
 #include <QtTest/QTest>
 #include <QtTest/QSignalSpy>
@@ -63,42 +61,6 @@ void JobsTest::deleteInitManagerJobTest()
     QCOMPARE(jobSpy2.count(), 0);
 }
 
-void JobsTest::deleteInitAdaptersJobTest()
-{
-    Manager *manager = new Manager();
-    manager->init()->exec();
-    QVERIFY(manager->isInitialized());
-
-    InitAdaptersJob *job = manager->initAdapters();
-    QSignalSpy jobSpy(job, SIGNAL(result(InitAdaptersJob*)));
-
-    job->start();
-    delete job;
-
-    if (jobSpy.count() == 0) {
-        jobSpy.wait(500);
-    }
-    // Deleting the job should emit error
-    QCOMPARE(jobSpy.count(), 1);
-
-    delete manager;
-
-    manager = new Manager();
-    manager->init()->exec();
-    QVERIFY(manager->isInitialized());
-
-    job = manager->initAdapters();
-    QSignalSpy jobSpy2(job, SIGNAL(result(InitAdaptersJob*)));
-
-    job->start();
-    job->kill();
-
-    jobSpy2.wait(500);
-    // Killing the job should NOT emit error
-    QCOMPARE(jobSpy2.count(), 0);
-
-}
-
 void JobsTest::deleteObexManagerTest()
 {
     ObexManager *manager = new ObexManager();
@@ -142,37 +104,6 @@ void JobsTest::deleteInitObexManagerJobTest()
     jobSpy2.wait(500);
     // Killing the job should NOT emit error
     QCOMPARE(jobSpy2.count(), 0);
-}
-
-void JobsTest::deleteLoadDeviceJobTest()
-{
-    Manager *manager = new Manager();
-    manager->init(Manager::InitManagerAndAdapters)->exec();
-    QVERIFY(manager->isInitialized());
-
-    Q_FOREACH (Device *device, manager->devices()) {
-        LoadDeviceJob *job = device->load();
-        QSignalSpy jobSpy(job, SIGNAL(result(LoadDeviceJob*)));
-
-        job->start();
-        delete job;
-
-        if (jobSpy.count() == 0) {
-            jobSpy.wait(500);
-        }
-        // Deleting the job should emit error
-        QCOMPARE(jobSpy.count(), 1);
-
-        job = device->load();
-        QSignalSpy jobSpy2(job, SIGNAL(result(LoadDeviceJob*)));
-
-        job->start();
-        job->kill();
-
-        jobSpy2.wait(500);
-        // Killing the job should NOT emit error
-        QCOMPARE(jobSpy2.count(), 0);
-    }
 }
 
 QTEST_MAIN(JobsTest)

autotests/jobstest.h
@@ -12,10 +12,8 @@ private Q_SLOTS:
 
     void deleteManagerTest();
     void deleteInitManagerJobTest();
-    void deleteInitAdaptersJobTest();
     void deleteObexManagerTest();
     void deleteInitObexManagerJobTest();
-    void deleteLoadDeviceJobTest();
 };
 
 #endif // JOBSTEST_H

src/CMakeLists.txt
@@ -9,8 +9,6 @@ set(qbluez_SRCS
     job.cpp
     initmanagerjob.cpp
     initobexmanagerjob.cpp
-    initadaptersjob.cpp
-    loaddevicejob.cpp
     utils.cpp
     utils_p.cpp
     agent.cpp
@@ -71,8 +69,6 @@ ecm_generate_headers(QBluez_HEADERS
         Job
         InitManagerJob
         InitObexManagerJob
-        InitAdaptersJob
-        LoadDeviceJob
         Utils
         Agent
         PendingCall

src/adapter.cpp
@@ -7,9 +7,9 @@
 namespace QBluez
 {
 
-Adapter::Adapter(const QString &path, QObject *parent)
+Adapter::Adapter(const QString &path, const QVariantMap &properties, QObject *parent)
     : QObject(parent)
-    , d(new AdapterPrivate(path, this))
+    , d(new AdapterPrivate(path, properties, this))
 {
 }
 
@@ -126,16 +126,8 @@ QList<Device*> Adapter::devices() const
 
 Device *Adapter::deviceForAddress(const QString &address) const
 {
-    // UBI will be /org/bluez/ADAPTER/dev_ADDRESS
-    //  * adapter ubi = /org/bluez/xxx
-    //  * address = ':' replaced with '_'
-
-    QString bluezAddress = address;
-    bluezAddress.replace(QLatin1Char(':'), QLatin1Char('_'));
-    const QString &deviceUbi = QString(QStringLiteral("%1/dev_%2")).arg(ubi(), bluezAddress);
-
     Q_FOREACH (Device *device, d->m_devices) {
-        if (device->ubi() == deviceUbi) {
+        if (device->address() == address) {
             return device;
         }
     }

src/adapter.h
@@ -98,7 +98,7 @@ Q_SIGNALS:
     void deviceChanged(Device *device);
 
 private:
-    explicit Adapter(const QString &address, QObject *parent);
+    explicit Adapter(const QString &address, const QVariantMap &properties, QObject *parent);
 
     class AdapterPrivate *const d;
 

commit 6060ced5c3fc2762841bbb3b9b9d03a206c33b28

Fix setting usable adapter on manager init
Regression from 0b50f8ab4a6a22189dfbdf9cd60a65c5adc69007

src/manager_p.cpp
@@ -135,6 +135,8 @@ void ManagerPrivate::getManagedObjectsFinished(QDBusPendingCallWatcher *watcher)
     }
 
     m_loaded = true;
+    m_usableAdapter = findUsableAdapter();
+
     Q_EMIT initFinished();
     Q_EMIT q->operationalChanged(true);
 }