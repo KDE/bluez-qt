3c14aaddf3746438864c3300500e7a18620efa67
David Rosca <nowrep@gmail.com>
2014-09-09 19:04:41 +0200

    Doc: Document Request class

--- src/request.h
+++ src/request.h
@@ -17,30 +17,68 @@ QBLUEZ_EXPORT void qbluez_acceptRequest(AgentType type, const QVariant &val, con
 QBLUEZ_EXPORT void qbluez_rejectRequest(AgentType type, const QDBusMessage &req);
 QBLUEZ_EXPORT void qbluez_cancelRequest(AgentType type, const QDBusMessage &req);
 
+/**
+ * ObexAgent/Agent request.
+ *
+ * This class represents a request from an agent. It is a convenient wrapper around
+ * QDBusMessage and easily allows sending replies and handling errors.
+ */
 template<typename T = void>
 class Request
 {
 public:
+    /**
+     * Constructs a new Request object.
+     */
     Request()
     {
     }
 
+    /**
+     * Constructs a new Request object.
+     *
+     * @param type type of agent
+     * @param message message used for reply
+     */
     Request(AgentType type, const QDBusMessage &message)
         : m_type(type)
         , m_message(message)
     {
     }
 
+    /**
+     * Accepts the request.
+     *
+     * This method should be called to send a reply to indicate
+     * the request was accepted.
+     *
+     * In case the request is of type void, this method does not
+     * take any parameter.
+     *
+     * @param returnValue return value of request
+     */
     void accept(T returnValue) const
     {
         qbluez_acceptRequest(m_type, returnValue, m_message);
     }
 
+    /**
+     * Rejects the request.
+     *
+     * This method should be called to send an error reply to
+     * indicate the request was rejected.
+     */
     void reject() const
     {
         qbluez_rejectRequest(m_type, m_message);
     }
 
+    /**
+     * Cancels the request.
+     *
+     * This method should be called to send an error reply to
+     * indicate the request was canceled.
+     */
     void cancel() const
     {
         qbluez_cancelRequest(m_type, m_message);
