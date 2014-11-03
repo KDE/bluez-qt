63e2e4933ea4a961c9e08b2f7aed49c64273a5e5
David Rosca <nowrep@gmail.com>
2014-11-03 10:58:41 +0100

    Services: Add A2DP service UUID

--- src/utils.h
+++ src/utils.h
@@ -38,6 +38,7 @@ const QString ObexObjectPush = QLatin1String("00001105-0000-1000-8000-00805F9B34
 const QString ObexFileTransfer = QLatin1String("00001106-0000-1000-8000-00805F9B34FB");
 const QString AudioSource = QLatin1String("0000110A-0000-1000-8000-00805F9B34FB");
 const QString AudioVideoRemoteControlTarget = QLatin1String("0000110C-0000-1000-8000-00805F9B34FB");
+const QString AdvancedAudioDistributionProtocol = QLatin1String("0000110D-0000-1000-8000-00805F9B34FB");
 const QString AudioVideoRemoteControl = QLatin1String("0000110E-0000-1000-8000-00805F9B34FB");
 const QString HeadsetAudioGateway = QLatin1String("00001112-0000-1000-8000-00805F9B34FB");
 const QString Nap = QLatin1String("00001116-0000-1000-8000-00805F9B34FB");
