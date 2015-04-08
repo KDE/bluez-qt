2a799d5e4a7abaea58635dbc6644ebce31eff3d4
David Rosca <nowrep@gmail.com>
2015-04-08 21:19:02 +0200

    Services: Add few new service uuids + rename PhonebookAccessPse
    New services: ServiceDiscoveryServer, Headset, Panu, Handsfree
    Renamed: PhonebookAccessPse -> PhonebookAccessServer

--- src/imports/bluezqtextensionplugin.cpp
+++ src/imports/bluezqtextensionplugin.cpp
@@ -40,20 +40,24 @@ static QJSValue services_singleton(QQmlEngine *engine, QJSEngine *scriptEngine)
     Q_UNUSED(engine)
 
     QJSValue object = scriptEngine->newObject();
+    object.setProperty(QStringLiteral("ServiceDiscoveryServer"), BluezQt::Services::ServiceDiscoveryServer);
     object.setProperty(QStringLiteral("SerialPort"), BluezQt::Services::SerialPort);
     object.setProperty(QStringLiteral("DialupNetworking"), BluezQt::Services::DialupNetworking);
     object.setProperty(QStringLiteral("ObexObjectPush"), BluezQt::Services::ObexObjectPush);
     object.setProperty(QStringLiteral("ObexFileTransfer"), BluezQt::Services::ObexFileTransfer);
+    object.setProperty(QStringLiteral("Headset"), BluezQt::Services::Headset);
     object.setProperty(QStringLiteral("AudioSource"), BluezQt::Services::AudioSource);
     object.setProperty(QStringLiteral("AudioVideoRemoteControlTarget"), BluezQt::Services::AudioVideoRemoteControlTarget);
     object.setProperty(QStringLiteral("AdvancedAudioDistribution"), BluezQt::Services::AdvancedAudioDistribution);
     object.setProperty(QStringLiteral("AudioVideoRemoteControl"), BluezQt::Services::AudioVideoRemoteControl);
     object.setProperty(QStringLiteral("HeadsetAudioGateway"), BluezQt::Services::HeadsetAudioGateway);
+    object.setProperty(QStringLiteral("Panu"), BluezQt::Services::Panu);
     object.setProperty(QStringLiteral("Nap"), BluezQt::Services::Nap);
+    object.setProperty(QStringLiteral("Handsfree"), BluezQt::Services::Handsfree);
     object.setProperty(QStringLiteral("HandsfreeAudioGateway"), BluezQt::Services::HandsfreeAudioGateway);
     object.setProperty(QStringLiteral("HumanInterfaceDevice"), BluezQt::Services::HumanInterfaceDevice);
     object.setProperty(QStringLiteral("SimAccess"), BluezQt::Services::SimAccess);
-    object.setProperty(QStringLiteral("PhonebookAccessPse"), BluezQt::Services::PhonebookAccessPse);
+    object.setProperty(QStringLiteral("PhonebookAccessServer"), BluezQt::Services::PhonebookAccessServer);
     object.setProperty(QStringLiteral("MessageAccessServer"), BluezQt::Services::MessageAccessServer);
     object.setProperty(QStringLiteral("PnpInformation"), BluezQt::Services::PnpInformation);
     return object;
--- src/services.h
+++ src/services.h
@@ -31,20 +31,24 @@ namespace BluezQt
 namespace Services
 {
 
+const QString ServiceDiscoveryServer = QLatin1String("00001000-0000-1000-8000-00805F9B34FB");
 const QString SerialPort = QLatin1String("00001101-0000-1000-8000-00805F9B34FB");
 const QString DialupNetworking = QLatin1String("00001103-0000-1000-8000-00805F9B34FB");
 const QString ObexObjectPush = QLatin1String("00001105-0000-1000-8000-00805F9B34FB");
 const QString ObexFileTransfer = QLatin1String("00001106-0000-1000-8000-00805F9B34FB");
+const QString Headset = QLatin1String("00001108-0000-1000-8000-00805F9B34FB");
 const QString AudioSource = QLatin1String("0000110A-0000-1000-8000-00805F9B34FB");
 const QString AudioVideoRemoteControlTarget = QLatin1String("0000110C-0000-1000-8000-00805F9B34FB");
 const QString AdvancedAudioDistribution = QLatin1String("0000110D-0000-1000-8000-00805F9B34FB");
 const QString AudioVideoRemoteControl = QLatin1String("0000110E-0000-1000-8000-00805F9B34FB");
 const QString HeadsetAudioGateway = QLatin1String("00001112-0000-1000-8000-00805F9B34FB");
+const QString Panu = QLatin1String("00001115-0000-1000-8000-00805F9B34FB");
 const QString Nap = QLatin1String("00001116-0000-1000-8000-00805F9B34FB");
+const QString Handsfree = QLatin1String("0000111E-0000-1000-8000-00805F9B34FB");
 const QString HandsfreeAudioGateway = QLatin1String("0000111F-0000-1000-8000-00805F9B34FB");
 const QString HumanInterfaceDevice = QLatin1String("00001124-0000-1000-8000-00805F9B34FB");
 const QString SimAccess = QLatin1String("0000112D-0000-1000-8000-00805F9B34FB");
-const QString PhonebookAccessPse = QLatin1String("0000112F-0000-1000-8000-00805F9B34FB");
+const QString PhonebookAccessServer = QLatin1String("0000112F-0000-1000-8000-00805F9B34FB");
 const QString MessageAccessServer = QLatin1String("00001132-0000-1000-8000-00805F9B34FB");
 const QString PnpInformation = QLatin1String("00001200-0000-1000-8000-00805F9B34FB");
 
