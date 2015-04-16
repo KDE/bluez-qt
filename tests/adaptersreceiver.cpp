f8bf0b355f4a5dcd1eec48ea2d868d8a5ddceba8
David Rosca <nowrep@gmail.com>
2015-04-16 11:27:57 +0200

    Add missing copyright headers to all source files

--- tests/adaptersreceiver.cpp
+++ tests/adaptersreceiver.cpp
@@ -1,25 +1,24 @@
-/*****************************************************************************
- * This file is part of the BluezQt project                                   *
- *                                                                           *
- * Copyright (C) 2011 Rafael Fernández López <ereslibre@kde.org>             *
- * Copyright (C) 2011 UFO Coders <info@ufocoders.com>                        *
- * Copyright (C) 2014 David Rosca <nowrep@gmail.com>                         *
- *                                                                           *
- * This library is free software; you can redistribute it and/or             *
- * modify it under the terms of the GNU Library General Public               *
- * License as published by the Free Software Foundation; either              *
- * version 2 of the License, or (at your option) any later version.          *
- *                                                                           *
- * This library is distributed in the hope that it will be useful,           *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
- * Library General Public License for more details.                          *
- *                                                                           *
- * You should have received a copy of the GNU Library General Public License *
- * along with this library; see the file COPYING.LIB.  If not, write to      *
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,      *
- * Boston, MA 02110-1301, USA.                                               *
- *****************************************************************************/
+/*
+ * Copyright (C) 2011 Rafael Fernández López <ereslibre@kde.org>
+ * Copyright (C) 2011 UFO Coders <info@ufocoders.com>
+ * Copyright (C) 2014-2015 David Rosca <nowrep@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "adaptersreceiver.h"
 
