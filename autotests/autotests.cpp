f8beee19f0cd43b5267a57e2bde89f6f43e17276
David Rosca <nowrep@gmail.com>
2017-08-13 13:18:52 +0200

    Autotests: Fix finding fakebluez binary
    BUG: 383453

--- autotests/autotests.cpp
+++ autotests/autotests.cpp
@@ -62,7 +62,7 @@ private:
 
 StartJob::StartJob()
     : QObject(nullptr)
-    , m_fakebluezPath(QCoreApplication::applicationDirPath() + QStringLiteral("/fakebluez"))
+    , m_fakebluezPath(QCoreApplication::applicationDirPath() + QStringLiteral("/fakebluez/fakebluez"))
 {
     // Fallback for old layout, can be removed after August 2017
     if (!QFile::exists(m_fakebluezPath))
