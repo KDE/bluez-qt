800f251877159a4645a38c56ba8a8a3d1e352161
Thiago Sueto <herzenschein@gmail.com>
2025-03-27 15:27:30 -0300

    QDoc Input

--- src/input.h
+++ src/input.h
@@ -19,27 +19,36 @@
 namespace BluezQt
 {
 /*!
- * @class BluezQt::Input input.h <BluezQt/Input>
- *
- * %Device input.
+ * \inmodule BluezQt
+ * \class BluezQt::Input
+ * \inheaderfile BluezQt/Input
+ * \brief Device input.
  *
  * This class represents an input interface.
  */
 class BLUEZQT_EXPORT Input : public QObject
 {
     Q_OBJECT
+    /*! \property ReconnectMode::reconnectMode */
     Q_PROPERTY(ReconnectMode reconnectMode READ reconnectMode NOTIFY reconnectModeChanged)
 
 public:
-    /*! Reconnect mode. */
+    /*!
+     * \enum BluezQt::Input::ReconnectMode
+     * \value NoReconnect
+     *        Device and host are not required to automatically restore the connection.
+     * \value HostReconnect
+     *        Host restores the connection.
+     * \value DeviceReconnect
+     *        Device restores the connection
+     * \value AnyReconnect
+     *        Device shall attempt to restore the lost connection,
+     *        but host may also restore the connection.
+     */
     enum ReconnectMode {
-        /*! Device and host are not required to automatically restore the connection. */
         NoReconnect,
-        /*! Host restores the connection. */
         HostReconnect,
-        /*! Device restores the connection. */
         DeviceReconnect,
-        /*! Device shall attempt to restore the lost connection, but host may also restore the connection. */
         AnyReconnect,
     };
     Q_ENUM(ReconnectMode)
@@ -51,21 +60,17 @@ public:
 
     /*!
      * Returns a shared pointer from this.
-     *
-     * @return InputPtr
      */
     InputPtr toSharedPtr() const;
 
     /*!
      * Returns the reconnect mode.
-     *
-     * @return reconnect mode
      */
     ReconnectMode reconnectMode() const;
 
 Q_SIGNALS:
     /*!
-     * Indicates that input's reconnect mode have changed.
+     * Indicates that the input's reconnect \a mode has changed.
      */
     void reconnectModeChanged(ReconnectMode mode);
 
