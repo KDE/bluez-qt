a1def0df05fb89a03ca328a7a73d7ef68d70b532
Thiago Sueto <herzenschein@gmail.com>
2025-03-27 10:04:51 -0300

    QDoc AgentAdaptor

--- src/agentadaptor.h
+++ src/agentadaptor.h
@@ -19,24 +19,49 @@ namespace BluezQt
 class Manager;
 class Agent;
 
+/*!
+ * \inmodule BluezQt
+ * \class BluezQt::AgentAdaptor
+ * \inheaderfile BluezQt/AgentAdaptor
+ */
 class AgentAdaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.bluez.Agent1")
 
 public:
+    /*!
+     */
     explicit AgentAdaptor(Agent *parent, Manager *manager);
 
 public Q_SLOTS:
+    /*!
+     */
     QString RequestPinCode(const QDBusObjectPath &device, const QDBusMessage &msg);
+    /*!
+     */
     Q_NOREPLY void DisplayPinCode(const QDBusObjectPath &device, const QString &pincode);
+    /*!
+     */
     quint32 RequestPasskey(const QDBusObjectPath &device, const QDBusMessage &msg);
+    /*!
+     */
     Q_NOREPLY void DisplayPasskey(const QDBusObjectPath &device, quint32 passkey, quint16 entered);
+    /*!
+     */
     void RequestConfirmation(const QDBusObjectPath &device, quint32 passkey, const QDBusMessage &msg);
+    /*!
+     */
     void RequestAuthorization(const QDBusObjectPath &device, const QDBusMessage &msg);
+    /*!
+     */
     void AuthorizeService(const QDBusObjectPath &device, const QString &uuid, const QDBusMessage &msg);
 
+    /*!
+     */
     Q_NOREPLY void Cancel();
+    /*!
+     */
     Q_NOREPLY void Release();
 
 private:
