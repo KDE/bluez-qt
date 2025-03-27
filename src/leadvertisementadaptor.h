ee91919858a7b07745a3e45d13ba9ce4ecf0c115
Thiago Sueto <herzenschein@gmail.com>
2025-03-27 17:10:34 -0300

    QDoc LEAdvertisingAdaptor

--- src/leadvertisementadaptor.h
+++ src/leadvertisementadaptor.h
@@ -16,25 +16,46 @@ namespace BluezQt
 {
 class LEAdvertisement;
 
+/*!
+ * \inmodule BluezQt
+ * \class BluezQt::LEAdvertisementAdaptor
+ * \inheaderfile BluezQt/LEAdvertisementAdaptor
+ */
 class LEAdvertisementAdaptor : public QDBusAbstractAdaptor
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.bluez.LEAdvertisement1")
+    /*! \property QString::Type */
     Q_PROPERTY(QString Type READ type)
+    /*! \property QStringList::ServiceUUIDs */
     Q_PROPERTY(QStringList ServiceUUIDs READ serviceUuids)
+    /*! \property QHash<QString,QVariant>::ServiceData */
     Q_PROPERTY(QHash<QString, QVariant> ServiceData READ serviceData)
+    /*! \property QHash<quint16,QDBusVariant>::ManufacturerData */
     Q_PROPERTY(QHash<quint16, QDBusVariant> ManufacturerData READ manufacturerData)
 
 public:
+    /*!
+     */
     explicit LEAdvertisementAdaptor(LEAdvertisement *parent);
 
+    /*!
+     */
     QString type() const;
 
+    /*!
+     */
     QStringList serviceUuids() const;
+    /*!
+     */
     QHash<QString, QVariant> serviceData() const;
+    /*!
+     */
     QHash<quint16, QDBusVariant> manufacturerData() const;
 
 public Q_SLOTS:
+    /*!
+     */
     Q_NOREPLY void Release();
 
 private:
